d4ad80d620ac167aca8a8fd1116a7585ab520f4a
@tuanthanh94 <tuanthanh021094@gmail.com>
2020-04-19 12:33:19 +0700

    Login completed

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,94 +1,110 @@
-import React, { Component } from "react";
-import { Button, Form, FormGroup, Input, Label } from "reactstrap";
-import "./Login.css";
-import { Link, Redirect } from "react-router-dom";
-import { CartContext } from "../../container/Cart/Cart";
-
+import React, {Component} from 'react';
+import {Button, Form, FormGroup, Input, Label} from 'reactstrap';
+import './Login.css';
+import {Link, Redirect} from 'react-router-dom';
+import {CartContext} from '../../container/Cart/Cart';
 
 class Login extends Component {
-  constructor(props) {
-    super(props);
-    const token = localStorage.getItem("token");
+  constructor (props) {
+    super (props);
+    const token = localStorage.getItem ('token');
     let loggedIn = true;
     if (token === null) {
       loggedIn = false;
     }
     this.state = {
-      username: "",
-      password: "",
-      loggedIn
+      username: '',
+      password: '',
+      loggedIn,
     };
 
-    this.onChange = this.onChange.bind(this);
-    this.onSubmitForm = this.onSubmitForm.bind(this);
+    this.onChange = this.onChange.bind (this);
+    this.onSubmitForm = this.onSubmitForm.bind (this);
   }
 
-  onChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value
+  onChange (e) {
+    this.setState ({
+      [e.target.name]: e.target.value,
     });
   }
 
-  onSubmitForm(e) {
-    e.preventDefault();
-    const { username, password } = this.state;
-    
+  onSubmitForm (callback) {
+    const {username, password} = this.state;
+
     // Login magic
-    
-    if (username === "1" && password === "1") {
-      localStorage.setItem("token", "sadnqwiojdosadm");
-      this.setState({ 
-        loggedIn: true
+
+    if (username === '1' && password === '1') {
+      localStorage.setItem ('token', 'sadnqwiojdosadm');
+      this.setState ({
+        loggedIn: true,
       });
+      callback()
     } else {
-      alert('Bạn đã nhập sai tài khoản...')
+      alert ('Bạn đã nhập sai tài khoản...');
     }
   }
 
-  render() {
+  render () {
     if (this.state.loggedIn) {
       return <Redirect to="/" />;
     }
     return (
       <div className="Login">
-        <Form className="Form" onSubmit={this.onSubmitForm}>
-          <h3 className='Sign-in'>Sign in</h3>
-          <img
-            src="https://www.now.vn/app/assets/img/Logo-Nowvn-Elip.png?e398e9326ddac9d81cae2dfce3c1d96c"
-            alt=""
-          />
-          <FormGroup className="FormGroup">
-            <Input
-              type="text"
-              placeholder="username"
-              name="username"
-              value={this.state.username}
-              onChange={this.onChange}
-            />
-          </FormGroup>
-          <FormGroup className="FormGroup">
-            <Input
-              type="password"
-              placeholder="password"
-              name="password"
-              value={this.state.password}
-              onChange={this.onChange}
-            />
-          </FormGroup>
-          <Label check className="typeCheckBox">
-            <Input type="checkbox" /> Remember me
-          </Label>
-          <CartContext.Consumer>
-            {({onLogin}) => (
+        <CartContext.Consumer>
+          {({onLogin}) => (
+            <Form className="Form" >
+              <h3 className="Sign-in">Sign in</h3>
+              <img
+                src="https://www.now.vn/app/assets/img/Logo-Nowvn-Elip.png?e398e9326ddac9d81cae2dfce3c1d96c"
+                alt=""
+              />
+              <FormGroup className="FormGroup">
+                <Input
+                  type="text"
+                  placeholder="username"
+                  name="username"
+                  value={this.state.username}
+                  onChange={this.onChange}
+                />
+              </FormGroup>
+              <FormGroup className="FormGroup">
+                <Input
+                  type="password"
+                  placeholder="password"
+                  name="password"
+                  value={this.state.password}
+                  onChange={this.onChange}
+                />
+              </FormGroup>
+              <Label check className="typeCheckBox">
+                <Input type="checkbox" /> Remember me
+              </Label>
+              <Button
+                type="submit"
+                className="Button"
+                onClick={() => this.onSubmitForm(onLogin)}
+              >
+                Sign in
+              </Button>
+              <div className="FooterLogin">
+                <Label>
+                  <Link
+                    style={{color: `white`, fontSize: `1rem`}}
+                    to="/register/"
+                  >
+                    Register
+                  </Link>
+                </Label>
+                <Label>
+                  <Link style={{color: `white`, fontSize: `1rem`}} to="#">
+                    Forgot password ?
+                  </Link>
+                </Label>
+              </div>
+            </Form>
+          )}
+        </CartContext.Consumer>
 
-              <Button type='submit' className="Button" onClick={onLogin}>Sign in</Button>
-              )}
-          </CartContext.Consumer>
-          <div className="FooterLogin">
-            <Label><Link style={{color: `white`, fontSize:`1rem`}} to="/register/">Register</Link></Label>
-            <Label><Link style={{color: `white`, fontSize:`1rem`}} to="#">Forgot password ?</Link></Label>
-          </div>
-        </Form>
       </div>
     );
   }
